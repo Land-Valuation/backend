cb2fc86f85f05a43194ac32efeeec425cfd2fe83
Hoang Do Minh <101560502+hoangharr@users.noreply.github.com>
2025-01-06 16:17:06 +0700

    update land valuation ui

---
+++ lvis-ui/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import { Box, Typography } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Box
+      sx={{
+        // mt: 2,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "8px",
+        // borderTop: "1px solid #F0F0F0",
+        padding: "16px 0 0 0",
+      }}
+    >
+      <Typography
+        sx={{
+          fontFamily: "Poppins",
+          fontSize: "16px",
+          fontWeight: 500,
+          lineHeight: "22px",
+          color: "#000000A6",
+        }}
+      >
+        Land Price Information System
+      </Typography>
+      <Typography
+        sx={{
+          backgroundColor: "#E6F4FF",
+          color: "#1677FF",
+          padding: "1px 8px",
+          borderRadius: "4px",
+          fontFamily: "Poppins",
+          fontSize: "12px",
+          fontWeight: 400,
+          lineHeight: "20px",
+        }}
+      >
+        In Lao PDR
+      </Typography>
+    </Box>
+  );
+};
+export default Footer;
